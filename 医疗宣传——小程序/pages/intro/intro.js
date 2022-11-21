0cf360438a7d3fff708fe663dfcaf11b2a1c58ff
Xie <2020151036@email.szu.edu.cn>
2022-11-21 20:52:11 +0800

    完成了首页的静态页面

--- 医疗宣传——小程序/pages/intro/intro.js
+++ 医疗宣传——小程序/pages/intro/intro.js
@@ -9,6 +9,19 @@ Page({
       pic3: '/rsc/u=4188778147,2019931444&fm=253&fmt=auto&app=120&f=JPEG.webp'
     },
     margin:"10px",
+    nickName:'',
+    comp:[
+      {title: '神医', 
+        widgets_left: [
+        {title: '小野', content: '主治医师', img: '/rsc/u=941056406,1822751651&fm=253&fmt=auto&app=120&f=JPEG.webp'},
+        {title: '小野', content: '主治医师', img: '/rsc/u=941056406,1822751651&fm=253&fmt=auto&app=120&f=JPEG.webp'},
+        {title: '小野', content: '主治医师', img: '/rsc/u=941056406,1822751651&fm=253&fmt=auto&app=120&f=JPEG.webp'}
+        ],
+        widgets_right: [
+        {title: '小野', content: '主治医师', img: '/rsc/u=941056406,1822751651&fm=253&fmt=auto&app=120&f=JPEG.webp'},
+        {title: '小野', content: '主治医师', img: '/rsc/u=941056406,1822751651&fm=253&fmt=auto&app=120&f=JPEG.webp'}
+        ],
+      }]
   },
   onClick(event) {
     this.setData({active:event.detail});
@@ -19,4 +32,11 @@ Page({
   onShow(event){
     wx.hideHomeButton();
   },
+  onLoad(){
+    if(getApp().globalData.hasUserInfo){
+      this.setData({
+        nickName:getApp().globalData.userInfo.nickName,
+      })
+    }
+  }
 })
